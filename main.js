90fb0817c9c0f296b3f58bb4c4c4f1c078eeeeaa
Tomas Rondon <tomaslr.code@gmail.com>
2017-05-02 09:36:55 -0400

    js file

---
+++ main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+
+   $('#phoneBtn').on('click',function(){
+   		console.log(("clicked"));
+   		$('#phone').html('<span>917-297-0466</span>').show();
+   });
+
+});
